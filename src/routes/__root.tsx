673bbdd2b1b4fe8b7bf7ced131143cedf0d106b0
Daniel Santos <danielbssantos2009@gmail.com>
2026-07-22 00:17:11 -0300

    fix build errors

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,8 +9,7 @@ import {
 } from "@tanstack/react-router";
 import { useEffect, type ReactNode } from "react";
 
-import appCss from "../styles.css?url";
-import { reportLovableError } from "../lib/lovable-error-reporting";
+import appCss from "../style.css?url";
 
 function NotFoundComponent() {
   return (
@@ -38,7 +37,7 @@ function ErrorComponent({ error, reset }: { error: Error; reset: () => void }) {
   console.error(error);
   const router = useRouter();
   useEffect(() => {
-    reportLovableError(error, { boundary: "tanstack_root_error_component" });
+    // Error reporting removed
   }, [error]);
 
   return (
